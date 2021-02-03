7e5f9110553641faa22c0a2954e1b33cb4a85c96
Lucas de souza lameira <lucasslameira@gmail.com>
2021-02-03 19:01:14 -0300

    5 dia finalizado

--- exquizme/pages/quiz/[id].js
+++ exquizme/pages/quiz/[id].js
@@ -1,8 +1,40 @@
-import { func } from 'prop-types'
-import React from 'react'
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import QuizScreen from '../../src/screens/Quiz';
 
-export default function QuizDaGalera () {
+export default function QuizDaGaleraPage({ dbExterno }) {
   return (
-    <h1>Quiz da galera page</h1>
-  )
+    <ThemeProvider theme={dbExterno.theme}>
+      <QuizScreen
+        externalQuestions={dbExterno.questions}
+        externalBg={dbExterno.bg}
+      />
+    </ThemeProvider>
+   
+  );
+}
+
+
+/* Configurações a nivel servidor - server side rendering */
+export async function getServerSideProps(context) {
+  const [projectName, githubUser] = context.query.id.split('___');
+
+  try {
+    const dbExterno = await fetch(`https://${projectName}.${githubUser}.vercel.app/api/db`)
+      .then((respostaDoServer) => {
+        if (respostaDoServer.ok) {
+          return respostaDoServer.json();
+        }
+        throw new Error('Falha em pegar os dados');
+      })
+      .then((respostaConvertidaEmObjeto) => respostaConvertidaEmObjeto)
+
+    return {
+      props: {
+        dbExterno,
+      },
+    };
+  } catch(err) {
+    throw new Error(err);
+  }
 }
